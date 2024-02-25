ca81b332c2eb73b6465657addb5ec599042d1489
Amir Hossein Mohammadi <101041110+amirhnmi@users.noreply.github.com>
2024-02-25 16:56:29 +0330

    Update database.py

--- dependencies/database.py
+++ dependencies/database.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+mysqlconnector://root:manamirhastam@127.0.0.1:3306/cinematicket-fastapi")
+engine = create_engine("mysql+mysqlconnector://***:***@127.0.0.1:3306/***")
 sessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
